492313f0f91d89a27261bf2fcb7090fe248300ba
vinolivae <viniciusoa@outlook.com>
2022-03-06 19:29:25 -0300

    chore: add new deps

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule Gituser.MixProject do
       {:tesla, "~> 1.4"},
       {:hackney, "~> 1.17"},
       {:bypass, "~> 2.1", only: :test},
-      {:mox, "~> 1.0", only: :test}
+      {:mox, "~> 1.0", only: :test},
+      {:pbkdf2_elixir, "~> 2.0"}
     ]
   end
 
